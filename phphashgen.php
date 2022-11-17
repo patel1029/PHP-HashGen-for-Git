ce66449d1acc7b7926363d3d9c2979fba94862ae
patel1029 <patel1029@gmail.com>
2022-11-17 11:32:40 -0600

    updated comments

--- phphashgen.php
+++ phphashgen.php
@@ -1,6 +1,6 @@
 <html>
 <?php
 echo "Testing 123m <br>";
-echo hash('md5', date("l jS \of F Y h:i:s A"));
+echo hash('md5', date("l jS \of F Y h:i:s A")); //echos date/time as a MD5 hash
 ?>
 </html>
